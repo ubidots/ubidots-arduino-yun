a4bbb042693a332f3d56d723941c3533d7f55832
AgustinPelaez <agustin.pelaez@ubidots.com>
2014-11-18 16:29:11 -0500

    Update ubidots_get.lua

--- Content/ubidots_get.lua
+++ Content/ubidots_get.lua
@@ -1,7 +1,7 @@
 token= arg[1]
 DEVICEID = arg[2]
 
-URL = "http://things.ubidots.com/api/v1.6/variables/" .. DEVICEID .."/values"
+URL = "http://things.ubidots.com/api/v1.6/variables/" .. DEVICEID .."/values?page_size=1"
 -- Library to read command output
 local io = require "io"
 
@@ -11,6 +11,8 @@ local http = require "socket.http"
 -- loading ltn12 and json libraries from luci framework
 local ltn12 = require "luci.ltn12"
 local json = require "luci.json"
+
+local response_body = {}
 local response, code, response_headers = http.request{
     url=URL,
     method = "GET",
